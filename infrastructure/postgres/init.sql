b177fde08189b15df42e97088472eedcf2a4ba72
TranThanhLam2005 <s4038329@rmit.edu.vn>
2025-12-18 14:29:22 +0700

    chore: update identity service submodule

--- infrastructure/postgres/init.sql
+++ infrastructure/postgres/init.sql
@@ -9,3 +9,19 @@ CREATE USER order_user WITH PASSWORD 'order_pass';
 GRANT ALL PRIVILEGES ON DATABASE identity_db TO identity_user;
 GRANT ALL PRIVILEGES ON DATABASE branch_db TO branch_user;
 GRANT ALL PRIVILEGES ON DATABASE order_db TO order_user;
+
+-- Grant schema permissions (required in PostgreSQL 15+)
+\c identity_db;
+GRANT ALL ON SCHEMA public TO identity_user;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO identity_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO identity_user;
+
+\c branch_db;
+GRANT ALL ON SCHEMA public TO branch_user;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO branch_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO branch_user;
+
+\c order_db;
+GRANT ALL ON SCHEMA public TO order_user;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO order_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO order_user;
