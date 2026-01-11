ee4bb75ea2951391eb0755fce7b4d9b6349a8e1d
TranThanhLam2005 <s4038329@rmit.edu.vn>
2026-01-11 18:03:30 +0700

    feat: implement CRUD for menu endpoint service submodule

--- infrastructure/postgres/init.sql
+++ infrastructure/postgres/init.sql
@@ -1,14 +1,17 @@
 CREATE DATABASE identity_db;
 CREATE DATABASE branch_db;
 CREATE DATABASE order_db;
+CREATE DATABASE menu_db;
 
 CREATE USER identity_user WITH PASSWORD 'identity_pass';
 CREATE USER branch_user WITH PASSWORD 'branch_pass';
 CREATE USER order_user WITH PASSWORD 'order_pass';
+CREATE USER menu_user WITH PASSWORD 'menu_pass';
 
 GRANT ALL PRIVILEGES ON DATABASE identity_db TO identity_user;
 GRANT ALL PRIVILEGES ON DATABASE branch_db TO branch_user;
 GRANT ALL PRIVILEGES ON DATABASE order_db TO order_user;
+GRANT ALL PRIVILEGES ON DATABASE menu_db TO menu_user;
 
 -- Grant schema permissions (required in PostgreSQL 15+)
 \c identity_db;
@@ -25,3 +28,9 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO branch_user;
 GRANT ALL ON SCHEMA public TO order_user;
 GRANT ALL ON ALL TABLES IN SCHEMA public TO order_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO order_user;
+
+\c menu_db;
+GRANT ALL ON SCHEMA public TO menu_user;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO menu_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO menu_user;
+
