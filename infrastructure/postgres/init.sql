166e5d47267ea220ee5e40d9f5937ac166edecf1
TranThanhLam2005 <s4038329@rmit.edu.vn>
2026-01-15 18:08:19 +0700

    chore: add user-db

--- infrastructure/postgres/init.sql
+++ infrastructure/postgres/init.sql
@@ -2,16 +2,20 @@ CREATE DATABASE identity_db;
 CREATE DATABASE branch_db;
 CREATE DATABASE order_db;
 CREATE DATABASE menu_db;
+CREATE DATABASE user_db;
 
 CREATE USER identity_user WITH PASSWORD 'identity_pass';
 CREATE USER branch_user WITH PASSWORD 'branch_pass';
 CREATE USER order_user WITH PASSWORD 'order_pass';
 CREATE USER menu_user WITH PASSWORD 'menu_pass';
+CREATE USER user_user WITH PASSWORD 'user_pass';
+
 
 GRANT ALL PRIVILEGES ON DATABASE identity_db TO identity_user;
 GRANT ALL PRIVILEGES ON DATABASE branch_db TO branch_user;
 GRANT ALL PRIVILEGES ON DATABASE order_db TO order_user;
 GRANT ALL PRIVILEGES ON DATABASE menu_db TO menu_user;
+GRANT ALL PRIVILEGES ON DATABASE user_db TO user_user;
 
 -- Grant schema permissions (required in PostgreSQL 15+)
 \c identity_db;
@@ -34,3 +38,7 @@ GRANT ALL ON SCHEMA public TO menu_user;
 GRANT ALL ON ALL TABLES IN SCHEMA public TO menu_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO menu_user;
 
+\c user_db;
+GRANT ALL ON SCHEMA public TO user_user;
+GRANT ALL ON ALL TABLES IN SCHEMA public TO user_user;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO user_user;
